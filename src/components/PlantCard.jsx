de50fafcb5c5df7253a1722fd79f193d3dca1a64
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-11 12:14:22 +0300

    added function to display each plant and handle sold out

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onSoldOut }) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      {/* plant image */}
+      <img src={plant.image} alt={plant.name} />
+
+      {/* plant name */}
+      <h4>{plant.name}</h4>
+
+      {/* price */}
+      <p>Price: {plant.price}</p>
+
+      
+      <button className="primary" onClick={() => onSoldOut(plant.id)}>{plant.inStock ? "Mark as Sold Out" : "Sold Out"}</button>
+      
     </li>
   );
 }
